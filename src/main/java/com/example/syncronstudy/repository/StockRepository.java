1582877c24933ab5141e2f6c13f1eda61249255b
ued123 <32861341+ued123@users.noreply.github.com>
2024-10-03 14:38:21 +0900

    optimistic lock

--- src/main/java/com/example/syncronstudy/repository/StockRepository.java
+++ src/main/java/com/example/syncronstudy/repository/StockRepository.java
@@ -1,7 +1,14 @@
 package com.example.syncronstudy.repository;
 
 import com.example.syncronstudy.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock,Long> {
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithOptimisticLock(Long id);
 }
---
+++ src/main/java/com/example/syncronstudy/service/OptimisticLockStockService.java
@@ -0,0 +1,24 @@
+package com.example.syncronstudy.service;
+
+import com.example.syncronstudy.domain.Stock;
+import com.example.syncronstudy.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OptimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public OptimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+
+        stock.decrease(quantity);
+        stockRepository.save(stock);
+    }
+}
