57e48eeb20bc12c258a8f3ed2bd11ba75fe52784
ued123 <32861341+ued123@users.noreply.github.com>
2024-10-03 14:59:06 +0900

    named 락 구현

---
+++ src/main/java/com/example/syncronstudy/facade/NamedLockStockFacade.java
@@ -0,0 +1,30 @@
+package com.example.syncronstudy.facade;
+
+import com.example.syncronstudy.repository.LockRepository;
+import com.example.syncronstudy.service.StockService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class NamedLockStockFacade {
+
+    private final LockRepository lockRepository;
+
+    private final StockService stockService;
+
+    public NamedLockStockFacade(LockRepository lockRepository, StockService stockService) {
+        this.lockRepository = lockRepository;
+        this.stockService = stockService;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+
+        try {
+            lockRepository.getLock(id.toString());
+            stockService.decrease(id,quantity);
+        } finally {
+            lockRepository.releaseLock(id.toString());
+        }
+    }
+}
